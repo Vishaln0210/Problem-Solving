caf690b65a49a181c15aaa0d079aba9ccd0b160e
Vishal N <153277270+Vishaln0210@users.noreply.github.com>
2024-10-27 13:10:21 +0530

    Time: 26 ms (80%), Space: 44.6 MB (34.77%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.java
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.java
@@ -1,3 +1,29 @@
+/*class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0; 
+        for(int i = low; i <= high; i++){
+            int length = (int) Math.log10(i) + 1;
+            String num = Integer.toString(i);
+            int firstsum = 0; 
+            int secondsum = 0; 
+            while(length % 2 == 0){
+            for(int j = 0; j < length/2; j++){
+               
+                firstsum+= (int)(num.charAt(j)); 
+            }
+            for(int k = length/2 + 1; k < length; k++){
+             
+                secondsum+= (int)(num.charAt(k)); 
+            }
+            if(firstsum == secondsum){
+                count++;
+            }
+            }
+                   
+        }
+        return count;
+    }
+}*/
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
         int count = 0; 
