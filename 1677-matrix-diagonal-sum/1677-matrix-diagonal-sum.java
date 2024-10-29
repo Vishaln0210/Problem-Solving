7ab4b6d050b77116dc6dffd69a0c5fb2779b9d97
Vishal N <153277270+Vishaln0210@users.noreply.github.com>
2024-10-30 00:29:23 +0530

    Time: 0 ms (100%), Space: 44.1 MB (64.21%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int diagonalSum(int[][] mat) {
         int sum = 0;
         int n = mat.length; 
@@ -22,4 +22,18 @@ class Solution {
         }
         return sum;
     }
+}*/
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0; 
+        for(int i = 0; i < n; i++){
+            sum += mat[i][i];
+            sum += mat[i][n-1-i];
+        }
+        if(n%2!=0){
+            sum-=mat[n/2][n/2];
+        }
+        return sum;
+}
 }
