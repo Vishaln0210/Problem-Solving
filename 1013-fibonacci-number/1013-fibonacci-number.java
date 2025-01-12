b9134d67476402967d64af0b0edf26cf4443384b
Vishal N <153277270+Vishaln0210@users.noreply.github.com>
2025-01-12 18:55:51 +0530

    Time: 8 ms (44.55%), Space: 40.3 MB (59.02%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,26 @@
+/*class Solution {
+    public int fib(int n){
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        if(n==0){
+            return 0;
+        }                                                                                     
+        while(n >= count){
+            
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+        return b;
+    }
+}*/
+class Solution {
+    public int fib(int n){
+        if(n < 2){
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+}
